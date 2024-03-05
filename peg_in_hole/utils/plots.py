54286b2bab300ec214b302914050667236d9ab61
Charles <charles.sirois14@gmail.com>
2024-03-05 16:10:46 -0500

    plots, wip

---
+++ peg_in_hole/utils/plots.py
@@ -0,0 +1,23 @@
+import h5py
+import numpy as np
+import random
+import math
+import matplotlib.pyplot as plt
+
+
+def make_plots():
+    # Load the data
+    runs_list = [248, 202, 203]
+
+    for each_run in runs_list:
+        ...
+
+    # Plots
+    training_reward_plot()
+
+
+def training_reward_plot(): ...
+
+
+if __name__ == '__main__':
+    make_plots()
